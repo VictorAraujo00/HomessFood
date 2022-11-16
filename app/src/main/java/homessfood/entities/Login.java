6b4655994f75657eccb145b43b5abe602e713299
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-15 21:16:33 -0300

    login

--- app/src/main/java/homessfood/entities/Login.java
+++ app/src/main/java/homessfood/entities/Login.java
@@ -1,5 +1,31 @@
 package homessfood.entities;
 
-public class Login {
+import java.util.Scanner;
+
+public class Login extends Pessoa {
+    private String nome="";
+    private String user="";
+    private int senha=0;
+    private Pessoa perfil = new Pessoa(nome, user, senha);
+    Scanner sc = new Scanner(System.in);
     
+
+    public Login(String nome, String user, int senha) {
+        super(nome, user, senha);
+    }
+
+    public Pessoa EntrarCozinheiro() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome de usuário: ");
+        String nomeInserido = sc.nextLine();
+        //nomeInserido = sc.nextLine();
+
+        System.out.println("Digite uma senha numérica: ");
+        int senhaInserida = sc.nextInt();
+        sc.nextLine();
+        user = "Cozinheiro";
+
+        Pessoa login = new Pessoa(nomeInserido, user, senhaInserida);
+        return login;
+    }
 }
