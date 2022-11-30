7ef1cd735907a34041075ea06950f816f4f3b342
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-30 19:55:05 +0000

    interface facade

---
+++ app/src/main/java/homessfood/Facade/IFacade.java
@@ -0,0 +1,8 @@
+package homessfood.Facade;
+
+public interface IFacade {
+    public void loginClientes() throws Exception;
+    public void loginCozinheiros() throws Exception;
+    public void cadastroClientes();
+    public void cadastroCozinheiros();
+}
