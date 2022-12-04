45661826bcb56268798267ca91aa6d388de87336
rayssadavvila <rayssamunizd@gmail.com>
2022-12-04 01:09:28 +0000

    Design pattern strategy ; interface pagamento

---
+++ app/src/main/java/homessfood/negocio/interfaces/PagamentoStrategy.java
@@ -0,0 +1,10 @@
+package homessfood.negocio.interfaces;
+
+public interface PagamentoStrategy {
+    void dadosDoPagamento();
+    //para a validação dos dados colocados
+    boolean validacao();
+    //quantia
+    void pagamentos(int amount);
+    
+}
