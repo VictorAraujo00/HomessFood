dc3a329e5c49c7372622078b0c7d68fac39bcd36
EduardaPires <pireseduarda526@gmail.com>
2022-12-04 08:23:30 -0300

    .

--- app/src/main/java/homessfood/application/Aplicacao.java
+++ app/src/main/java/homessfood/application/Aplicacao.java
@@ -42,8 +42,8 @@ public class Aplicacao {
     public static void main(String[] args) throws Exception {
         RepositorioPessoas novoRepositorio = new RepositorioPessoas();
         Facade fachadaApp = new Facade();
-        LinkedList<Pessoa> listaCozinheiros = new LinkedList<Pessoa>();
-        LinkedList<Pessoa> listaClientes = new LinkedList<Pessoa>();
+        //LinkedList<Pessoa> listaCozinheiros = new LinkedList<Pessoa>();
+        //LinkedList<Pessoa> listaClientes = new LinkedList<Pessoa>();
         Scanner sc = new Scanner(System.in);
         int op=0;
         //incialização do objeto Pessoa para chamar métodos das classes cozinheiros e cliente
@@ -51,66 +51,26 @@ public class Aplicacao {
         int senha=0;
         String[] cardapio = {};
         //chamando objetos das classes
-        Cozinheiros cozinheiros = new Cozinheiros(nome, user, senha, cardapio);
-        Cliente cliente = new Cliente(nome, user, senha);
-        Registro registro = new Registro(nome, user, senha, cardapio);
-        Login login = new Login(nome, user, senha);
+        //Cozinheiros cozinheiros = new Cozinheiros(nome, user, senha, cardapio);
+        //qCliente cliente = new Cliente(nome, user, senha);
+        //Registro registro = new Registro(nome, user, senha, cardapio);
+        //Login login = new Login(nome, user, senha);
         //criação de listas
 
         do {
             opcoes();
             op = sc.nextInt();
             if (op == 1) {
-            //   Pessoa cozinheiroLogin = login.EntrarCozinheiro();
-             /*  try{
-                    if(listaCozinheiros.contains(cozinheiroLogin)==false){
-                          throw new ExcecaoLogin();
-                      }
-                      else if(listaCozinheiros.contains(cozinheiroLogin)==true){
-                          System.out.println("Login efetuado com sucesso!");
-                           //achar posição do cozinheiro
-                        int pos = posNaLista(cozinheiroLogin, listaCozinheiros); //para que o cardapio corresponda ao cozinheiro, devemos salvar sua pos na lista
-                        cozinheiros.exibirTelaCozinheiro(pos);
-                       }
-                   }catch(ExcecaoLogin e){
-                      System.out.println("Usuário não corresponde /  Senha incorreta"); 
-                  } */
                   fachadaApp.loginCozinheiros();
               }
               else if (op== 2) {
-                //   Pessoa novoCozinheiro = registro.CadastroCozinheiro();
-                  /*if(listaCozinheiros.contains(novoCozinheiro)){
-                      System.out.println("Usuário já cadastrado!");
-                  }
-                  else{
-                      listaCozinheiros.addLast(novoCozinheiro);
-                      System.out.println("Cadastro feito com sucesso!");
-                  }*/
                   fachadaApp.cadastroCozinheiros();
               }
               else if (op == 3) {
-                  //Pessoa clienteLogin = login.EntrarCliente();
-                  
-                //   if(listaClientes.contains(clienteLogin)==false){
-                //     throw new ExcecaoLogin();
-                //   }
-                //     else if(listaClientes.contains(clienteLogin)==true){
-                //         System.out.println("Login efetuado com sucesso!");
-                //         System.out.println(" ");
-                //         cliente.opcoesTelaClientes(listaCozinheiros);
-                //     }
                 fachadaApp.loginClientes();
-               
               }
               else if (op== 4) {
-                //   Pessoa novoCliente = registro.CadastroCliente();             
-                //   if(listaClientes.contains(novoCliente)){
-                //       System.out.println("Usuário já cadastrado!");
-                //   }
-                //   else{
-                //       listaClientes.addLast(novoCliente);
-                //       System.out.println("Cadastro feito com sucesso!");
-                //   }
+              
                 fachadaApp.cadastroClientes();
               }
               else if (op == 5){
